2f15603b30dcdc73bc40d9c44eea366ea98af32d
madupree <madupree@iastate.edu>
2022-05-09 16:57:56 -0500

    Fix movement bugs, included delta in acceleration calcualtion

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,6 @@
 extends KinematicBody2D
 
 # Constant Variables
-const Movement = preload("Movement.gd")
 const BUTTON_UP = 0
 const BUTTON_DOWN = 1
 const BUTTON_LEFT = 0
@@ -9,25 +8,65 @@ const BUTTON_RIGHT = 1
 const SQRT_H = sqrt(1.0/2.0)
 
 # Onready Variables
-onready var movement = Movement.new(speed, accel)
 onready var most_recent_direction_button_y = BUTTON_UP
 onready var most_recent_direction_button_x = BUTTON_LEFT
 
 # Variables
 var motion = Vector2()
 var velocity = Vector2()
-export var speed = 400
-export var accel = 40
-export var decel = 400 #set decel = speed for instant stopping
-
-# Classes
-class Movement2:
-	func thing():
-		print("gamer")
+var speed = 400
+var accel = 1600    # "time to top speed" = speed / accel
 
 # Only handles movement at the moment
-# Collisions not working properly
 func _physics_process(_delta):
+	move(_delta)
+
+
+func _input(_event):
+	# Keep track of most recently pressed movement button.
+	if _event.is_action_pressed("game_up"):
+		most_recent_direction_button_y = BUTTON_UP
+	elif _event.is_action_pressed("game_left"):
+		most_recent_direction_button_x = BUTTON_LEFT
+	elif _event.is_action_pressed("game_down"):
+		most_recent_direction_button_y = BUTTON_DOWN
+	elif _event.is_action_pressed("game_right"):
+		most_recent_direction_button_x = BUTTON_RIGHT
+
+# Gradually increase player speed up to speed using accel
+func accelerate(vec, delta):
+	# Accelerate x
+	if (vec.x < 0):
+		if (velocity.x > 0): # if velocity is positive while trying to move negative, set velocity to 0
+			velocity.x = 0
+		if (velocity.x > vec.x * speed):
+			velocity.x -= accel * delta
+	elif (vec.x > 0):
+		if (velocity.x < 0): # if velocity is negative while trying to move positive, set velocity to 0
+			velocity.x = 0
+		if (velocity.x < vec.x * speed):
+			velocity.x += accel * delta
+	# Accelerate y
+	if (vec.y < 0):
+		if (velocity.y > 0): # if velocity is positive while trying to move negative, set velocity to 0
+			velocity.y = 0
+		if (velocity.y > vec.y * speed):
+			velocity.y -= accel * delta
+	elif (vec.y > 0):
+		if (velocity.y < 0): # if velocity is negative while trying to move positive, set velocity to 0
+			velocity.y = 0
+		if (velocity.y < vec.y * speed):
+			velocity.y += accel * delta
+	
+	# Deccelerate x
+	if (abs(velocity.x) > abs(vec.x) * speed):
+		velocity.x = vec.x * speed
+	# Deccelerate y
+	if (abs(velocity.y) > abs(vec.y) * speed):
+		velocity.y = vec.y * speed
+
+# Move the player based on input
+func move(_delta):
 	motion.x = -Input.get_action_strength("game_left") + Input.get_action_strength("game_right")
 	motion.y = -Input.get_action_strength("game_up") + Input.get_action_strength("game_down")
 	
@@ -57,53 +96,9 @@ func _physics_process(_delta):
 			motion.x = -1
 	
 	if (Input.is_action_pressed("game_down") || Input.is_action_pressed("game_up") || Input.is_action_pressed("game_left") || Input.is_action_pressed("game_right")):
-		accel(motion)
+		accelerate(motion, _delta)
 	else:
 		velocity = motion * 0
 	
 	# Set Motion
 	velocity = move_and_slide(velocity)
-
-
-func _input(_event):
-	# Keep track of most recently pressed movement button.
-	if _event.is_action_pressed("game_up"):
-		most_recent_direction_button_y = BUTTON_UP
-	elif _event.is_action_pressed("game_left"):
-		most_recent_direction_button_x = BUTTON_LEFT
-	elif _event.is_action_pressed("game_down"):
-		most_recent_direction_button_y = BUTTON_DOWN
-	elif _event.is_action_pressed("game_right"):
-		most_recent_direction_button_x = BUTTON_RIGHT
-
-
-func accel(vec):
-	# Accelerate x
-	if (vec.x < 0):
-		if (velocity.x > 0): # if velocity is positive while trying to move negative, set velocity to 0
-			velocity.x = 0
-		if (velocity.x > vec.x * speed):
-			velocity.x -= accel
-	elif (vec.x > 0):
-		if (velocity.x < 0): # if velocity is negative while trying to move positive, set velocity to 0
-			velocity.x = 0
-		if (velocity.x < vec.x * speed):
-			velocity.x += accel
-	# Accelerate y
-	if (vec.y < 0):
-		if (velocity.y > 0): # if velocity is positive while trying to move negative, set velocity to 0
-			velocity.y = 0
-		if (velocity.y > vec.y * speed):
-			velocity.y -= accel
-	elif (vec.y > 0):
-		if (velocity.y < 0): # if velocity is negative while trying to move positive, set velocity to 0
-			velocity.y = 0
-		if (velocity.y < vec.y * speed):
-			velocity.y += accel
-	
-	# Deccelerate x
-	if (abs(velocity.x) > abs(vec.x) * speed):
-		velocity.x = vec.x * speed
-	# Deccelerate y
-	if (abs(velocity.y) > abs(vec.y) * speed):
-		velocity.y = vec.y * speed
